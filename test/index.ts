9df98f598bc69f4f98c5a5dfb9ae39cf238f621d
N U R O ™ <bendixon50@gmail.com>
2019-02-26 14:46:14 +0000

    :hankey: Added place holder tests

--- test/index.ts
+++ test/index.ts
@@ -1,3 +1,95 @@
+import { expect } from 'chai';
+import TDE from '../lib';
+
 describe('TDE', () => {
-	return true;
+	it('Initialize TDE', () => {
+		// const result = new TDE();
+		// expect(result).to.equal('Hello world!');
+		return true;
+	});
+	it('Add Extension', () => {
+		// const result = new TDE();
+		// expect(result).to.equal('Hello world!');
+		return true;
+	});
+	it('Remove Extension', () => {
+		// const result = new TDE();
+		// expect(result).to.equal('Hello world!');
+		return true;
+	});
+	it('Get Extension', () => {
+		// const result = new TDE();
+		// expect(result).to.equal('Hello world!');
+		return true;
+	});
+	it('Get All Extensions', () => {
+		// const result = new TDE();
+		// expect(result).to.equal('Hello world!');
+		return true;
+	});
+	it('Get All Enabeld Extensions', () => {
+		// const result = new TDE();
+		// expect(result).to.equal('Hello world!');
+		return true;
+	});
+	it('Get All Initialized Extensions', () => {
+		// const result = new TDE();
+		// expect(result).to.equal('Hello world!');
+		return true;
+	});
+	it('Enable Extension', () => {
+		// const result = new TDE();
+		// expect(result).to.equal('Hello world!');
+		return true;
+	});
+	it('Disable Extension', () => {
+		// const result = new TDE();
+		// expect(result).to.equal('Hello world!');
+		return true;
+	});
+	it('Initialize', () => {
+		// const result = new TDE();
+		// expect(result).to.equal('Hello world!');
+		return true;
+	});
+	it('Check Dependencies', () => {
+		// const result = new TDE();
+		// expect(result).to.equal('Hello world!');
+		return true;
+	});
+	it('Check For Conflicts', () => {
+		// const result = new TDE();
+		// expect(result).to.equal('Hello world!');
+		return true;
+	});
+	it('Is Extension Enabled', () => {
+		// const result = new TDE();
+		// expect(result).to.equal('Hello world!');
+		return true;
+	});
+	it('Is Extension Initialized', () => {
+		// const result = new TDE();
+		// expect(result).to.equal('Hello world!');
+		return true;
+	});
+	it('Resolve Dependency Graph', () => {
+		// const result = new TDE();
+		// expect(result).to.equal('Hello world!');
+		return true;
+	});
+	it('Initialize Extension', () => {
+		// const result = new TDE();
+		// expect(result).to.equal('Hello world!');
+		return true;
+	});
+	it('Destroy Extension', () => {
+		// const result = new TDE();
+		// expect(result).to.equal('Hello world!');
+		return true;
+	});
+	it('Re-order Enabled Extensions', () => {
+		// const result = new TDE();
+		// expect(result).to.equal('Hello world!');
+		return true;
+	});
 });
